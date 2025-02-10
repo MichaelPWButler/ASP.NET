aa79486290f70fdfdc5bd57255124598c43032b1
Michael Butler <michaelbut12345670@gmail.com>
2025-02-10 23:00:16 +0000

    Minor Refactoring Of Model And Updating Schema

--- Movies Tutorial Application/Movies Tutorial Application/Models/Movie.cs
+++ Movies Tutorial Application/Movies Tutorial Application/Models/Movie.cs
@@ -7,26 +7,20 @@ namespace Movies_Tutorial_Application.Models
     {
         public int Id { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
+        [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; } = string.Empty;
 
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date), Required]
         public DateTime ReleaseDate { get; set; }
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(1, 100), DataType(DataType.Currency), Required]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [Required]
-        [StringLength(30)]
-        public string Genre { get; set; } = string.Empty;
-
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
--- Movies Tutorial Application/Movies Tutorial Application/obj/Debug/net8.0/Movies Tutorial Application.AssemblyInfo.cs
+++ Movies Tutorial Application/Movies Tutorial Application/obj/Debug/net8.0/Movies Tutorial Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies Tutorial Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2006c67fe9059d30a030fbf67bc8f31b75b8ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d728744634539c5bf500886a32c857bac703c68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies Tutorial Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies Tutorial Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
