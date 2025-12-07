2b1a21fbf8337d651e611b44a1011b304a299f75
Michael Butler <michaelbut12345670@gmail.com>
2025-12-07 17:21:19 +0000

    Updated Logic for country model

--- LessOrMoreGame/Models/Country.cs
+++ LessOrMoreGame/Models/Country.cs
@@ -1,6 +1,16 @@
-﻿namespace LessOrMoreGame.Models
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LessOrMoreGame.Models
 {
     public class Country
     {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Population { get; set; }
+        [JsonPropertyName("FlagImgSrc")]
+        public string? ImageSource { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Country>(this);
     }
 }
