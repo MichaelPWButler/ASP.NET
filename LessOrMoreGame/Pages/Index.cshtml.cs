b709e38a296e12e0cdfcb2de7230addce0cc348d
Michael Butler <michaelbut12345670@gmail.com>
2025-12-07 18:36:09 +0000

    Using new service in Index Page

--- LessOrMoreGame/Pages/Index.cshtml.cs
+++ LessOrMoreGame/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using LessOrMoreGame.Models;
+using LessOrMoreGame.wwwroot.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +8,20 @@ namespace LessOrMoreGame.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileCountryService _JsonFileCountryService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileCountryService jsonFileCountryService)
         {
             _logger = logger;
+            _JsonFileCountryService = jsonFileCountryService;
         }
 
         public void OnGet()
         {
-
+            Countries = _JsonFileCountryService.GetCountries();
         }
+
+        public IEnumerable<Country> Countries { get; private set; } = [];
     }
 }
