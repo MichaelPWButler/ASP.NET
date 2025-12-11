56c885670ac46093be6970909d8ed77615cf03c6
Michael Butler <michaelbut12345670@gmail.com>
2025-12-11 22:54:59 +0000

    Adding functionality for green overlay

---
+++ LessOrMoreGame/Pages/Index.cshtml.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+const control_Country1 = document.getElementById('Country1'),
+    control_Overlay = document.getElementById('overlay');
+
+control_Country1.addEventListener('click', function () {
+    overlay.classList.add('active');
+
+    setTimeout(() => {
+        overlay.classList.remove('active');
+    }, 1000);
+});
