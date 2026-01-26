aa4da941c1d7b051a417989fb8e0761dd25841b7
Michael Butler <michaelbut12345670@gmail.com>
2026-01-26 00:19:20 +0000

    New Application

---
+++ InvestmentApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InvestmentApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
